9bf3832402785ceb4fab96a89c0e69762352333a
Jonas <jonascanaza@gmail.com>
2025-11-28 00:28:56 -0300

    Add click detection to the gameplay screen

--- src/screens/gameplay_screen.cpp
+++ src/screens/gameplay_screen.cpp
@@ -1,14 +1,33 @@
 #include "screens/gameplay_screen.h"
 
+#include <iostream>
+
+#include <SFML/Graphics.hpp>
+
 namespace Gameplay
 {
+	static bool leftWasPressed;
+
 	void Init()
 	{
-		
+		leftWasPressed = false;
 	}
 
 	void Input()
 	{
+		if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left))
+		{
+			if (!leftWasPressed)
+			{
+				std::cout << "Click!" << std::endl;
+
+				leftWasPressed = true;
+			}
+		}
+		else
+		{
+			leftWasPressed = false;
+		}
 
 	}
 
